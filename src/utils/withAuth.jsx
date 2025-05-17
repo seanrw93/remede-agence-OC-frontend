d63a5cf581ea74f37a769cb823b4dc8125c59c95
seanrw93 <seanrw@gmail.com>
2025-05-17 12:22:41 +0200

    Update readme

--- src/utils/withAuth.jsx
+++ src/utils/withAuth.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { Spinner } from "../components/Spinner"
