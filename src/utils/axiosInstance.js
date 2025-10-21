15a664907ce921f3273c66126673fec6422e8b13
seanrw93 <seanrw@gmail.com>
2025-10-21 18:52:30 +0200

    feat: update Axios instance base URL to include remote API instead of local

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3001/api/v1",
+    baseURL: "https://project-10-bank-api-78ns.onrender.com/api/v1/",
     headers: {
         "Content-Type": "application/json"
     }
